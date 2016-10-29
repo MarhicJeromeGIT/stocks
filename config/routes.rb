4144b8a793910e5611d0c0aa89fe9954690cb9ca
Jerome <marhic.jerome@gmail.com>
2016-10-29 11:17:52 +0200

    Styling and front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#welcome'
+  
+  get 'welcome', to: 'static_pages#welcome'
+  get 'about', to: 'static_pages#about'
+  
   resources :stocks
   resources :alerts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
