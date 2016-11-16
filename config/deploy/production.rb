25f3d96df24426511d92dbfa12393f3dcb41e9ba
Ubuntu <ubuntu@ip-172-31-15-31.eu-west-1.compute.internal>
2016-11-16 23:11:05 +0000

    Change server ip to elastic ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'ec2-52-211-249-61.eu-west-1.compute.amazonaws.com', user: 'ubuntu', roles: %w{app db web}
+server '52.211.249.61', user: 'ubuntu', roles: %w{app db web}
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
