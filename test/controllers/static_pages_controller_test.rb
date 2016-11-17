87fb38ca86417959b250f4f19043b7646a217fd7
Ubuntu <ubuntu@ip-172-31-15-31.eu-west-1.compute.internal>
2016-11-17 10:10:50 +0000

    Create a failing test to test jenkins

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+     assert false
+  end
 end
