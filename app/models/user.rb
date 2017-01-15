e1e063168650452be1641fae058b38e72ec44448
jerome <marhic.jerome@gmail.com>
2017-01-15 06:17:40 +0000

    Add swagger-blocks gem to generate swagger api definitions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,20 @@
 class User < ApplicationRecord
+  include Swagger::Blocks
+
+  swagger_schema :Pet do
+    key :required, [:id, :name]
+    property :id do
+      key :type, :integer
+      key :format, :int64
+    end
+    property :name do
+      key :type, :string
+    end
+    property :tag do
+      key :type, :string
+    end
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
